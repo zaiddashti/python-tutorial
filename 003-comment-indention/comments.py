f1fea43b6d2afc506bcca6e47c6af7f8e954b4bd
Zaid <zaiddashti@gmail.com>
2021-03-17 06:57:11 -0400

    002 and 003 are added

---
+++ 003-comment-indention/comments.py
@@ -0,0 +1,35 @@
+# This is a comment for a single line
+
+'''
+This
+is
+multi-lines
+comment
+'''
+
+#ِ Examples
+
+# This line prints output "Welcome 1!" on the terminal
+print("Welcome 1!")
+
+
+print("Welcome 2!") # This line prints output "Welcome 2!" on the terminal
+
+
+#print("Commented statement")
+print("Not commented statement")
+
+
+# This is a comment
+# for printing an output
+# The output is "Welcome 3!"
+print("Welcome 3!")
+
+
+
+'''
+This is a comment
+for printing an output
+The output is "Welcome 4!"
+'''
+print("Welcome 4!")
---
+++ 003-comment-indention/indention.py
@@ -0,0 +1,28 @@
+# correct
+print("Line 1")
+
+
+# wrong indention
+print("Line 2")
+    print("Line 3")
+
+
+# correct indention
+if 5 > 4:
+    print("It true that 5 > 4")
+
+
+# wrong indention
+if 5 > 4:
+print("It true that 5 > 4")
+
+
+# wrong indention
+    if 5 > 4:
+    print("It true that 5 > 4")
+
+
+# wrong indention
+if 5 > 4:
+    print("It true that 5 > 4")
+        print("It true that 5 > 4")
